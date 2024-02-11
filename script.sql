09ce4d3e1ef40bee4087a32dbb68493702eefb78
Breno Santos <brenosantos145@gmail.com>
2024-02-11 13:58:13 -0300

    feat: improve sql

--- script.sql
+++ script.sql
@@ -1,14 +1,28 @@
--- Coloque scripts iniciais aqui
-CREATE TABLE IF NOT EXISTS clientes (
-  id SERIAL PRIMARY KEY,
-  nome VARCHAR(255) NOT NULL,
-  limite INTEGER NOT NULL
-);
+DO $$
+BEGIN 
+    -- Criação de tabelas
+    CREATE TABLE IF NOT EXISTS clientes (
+        id SERIAL PRIMARY KEY NOT NULL,
+        nome VARCHAR(50) NOT NULL,
+        limite INTEGER NOT NULL,
+        saldo INTEGER NOT NULL
+    );
 
-INSERT INTO clientes (nome, limite)
-VALUES
-  ('o barato sai caro', 1000 * 100),
-  ('zan corp ltda', 800 * 100),
-  ('les cruders', 10000 * 100),
-  ('padaria joia de cocaia', 100000 * 100),
-  ('kid mais', 5000 * 100);
+    CREATE TABLE IF NOT EXISTS transacoes (
+        id SERIAL PRIMARY KEY NOT NULL,
+        tipo CHAR(1) NOT NULL,
+        descricao VARCHAR(10) NOT NULL,
+        valor INTEGER NOT NULL,
+        cliente_id INTEGER NOT NULL,
+        realizada_em TIMESTAMP NOT NULL DEFAULT NOW()
+    );
+
+    -- Inserção de valores iniciais na tabela clientes
+    INSERT INTO clientes (nome, limite, saldo)
+    VALUES
+        ('Isadora', 100000, 0),
+        ('Maicon', 80000, 0),
+        ('Matias', 1000000, 0),
+        ('Bob', 10000000, 0),
+        ('Tom', 500000, 0);
+END $$;
